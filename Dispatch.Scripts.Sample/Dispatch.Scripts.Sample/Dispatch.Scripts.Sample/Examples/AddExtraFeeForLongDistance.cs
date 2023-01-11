785c98eb45c10e824944324a27258c70166b0548
aproteau <alex.proteau@dispatchscience.com>
2023-01-11 20:44:32 +0000

    AUTO: Copying from DS Repo

---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddExtraFeeForLongDistance.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample.Examples
+{
+    public class AddExtraFeeForLongDistance : IOrderUpdateScript
+    {
+        public async Task OnOrderUpdate(IOrderUpdater order, IScriptDataProvider data, ILogger logger)
+        {
+            if (order.PickupAddress == null || order.DeliveryAddress == null)
+            {
+                return;
+            }
+            var pickupZone = await data.GetZones(ScriptZoneType.Script, order.PickupAddress.Location);
+            var deliveryZone = await data.GetZones(ScriptZoneType.Script, order.DeliveryAddress.Location);
+            var validZones = new string[] { "1_00", "2_00", "3_00", "4_00", "5_00", "6_00", "700_00" };
+            decimal over40 = 0;
+            decimal over30 = 0;
+            if (validZones.Contains(pickupZone.FirstOrDefault()) && validZones.Contains(deliveryZone.FirstOrDefault()))
+            {
+                if (!order.Attributes.Any(x => x.ToLower() == "noextramileagefee"))
+                {
+                    if (order.Distance.Value > 40 && order.Attributes.Any(x => x.ToLower() == "billover40"))
+                    {
+                        over40 = 1;
+                    }
+                    else if (order.Distance.Value > 30)
+                    {
+                        over30 = 1;
+                        return;
+                    }
+                }
+
+                var over40MilesExtra = order.ExtraFees.FirstOrDefault(e => string.Equals(e.ExtraFeeTypeId, "over40miles", StringComparison.InvariantCultureIgnoreCase));
+                if (over40MilesExtra == null)
+                {
+                    if (over40 > 0)
+                    {
+                        await order.AddExtraFee("Over40Miles", over40);
+                    }
+                }
+                else
+                {
+                    await order.UpdateExtraFee(over40MilesExtra.ChargeId, over40);
+                }
+
+
+                var over30MilesExtra = order.ExtraFees.FirstOrDefault(e => string.Equals(e.ExtraFeeTypeId, "over30miles", StringComparison.InvariantCultureIgnoreCase));
+                if (over30MilesExtra == null)
+                {
+                    if (over30 > 0)
+                    {
+                        await order.AddExtraFee("Over30Miles", over30);
+                    }
+                }
+                else
+                {
+                    await order.UpdateExtraFee(over30MilesExtra.ChargeId, over30);
+                }
+
+                await order.UpdateExtraFee(over30MilesExtra.ChargeId, Convert.ToDecimal(order.Distance.Value));
+                await order.UpdateExtraFee(over40MilesExtra.ChargeId, Convert.ToDecimal(order.Distance.Value));
+            }
+        }
+    }
+}
---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddHubBasedServiceLevel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample.Examples
+{
+    public class AddHubBasedServiceLevel : IOrderUpdateScript
+    {
+        public async Task OnOrderUpdate(IOrderUpdater order, IScriptDataProvider data, ILogger logger)
+        {
+            if (order.FulfillmentType != OrderFulfillmentType.Standard)
+            {
+                // We can only add hubs to a Standard order
+                return;
+            }
+
+            var scriptData = await data.GetScriptData();
+            if (!scriptData.Any())
+            {
+                // This script needs data to work
+                return;
+            }
+
+            var accountIds = scriptData["AccountIds"].Split(",");
+            var serviceLevels = scriptData["ServiceLevelIds"].Split(",");            
+            var lat = double.Parse(scriptData["HubLocation"].Split(',')[0]);
+            var lng = double.Parse(scriptData["HubLocation"].Split(',')[1]);
+
+            var warehouse = new HubInfo
+            {
+                Address = new Address
+                {
+                    AddressLine1 = scriptData["HubAddressLine1"],
+                    AddressLine2 = scriptData["HubAddressLine2"],
+                    City = scriptData["HubCity"],
+                    ZipPostalCode = scriptData["HubZip"],
+                    StateProvince = scriptData["HubProvice"],
+                    Company = scriptData["HubCompany"],
+                    Location = new LatLng(lat, lng)
+                },
+                Contact = new ContactInfo { Name = scriptData["HubContactName"], PhoneNumber = scriptData["HubContactPhone"] }
+            };
+
+            if (!serviceLevels.Any(x => string.Equals(x?.Trim(), order.ServiceLevelTypeId, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                return;
+            }
+
+            if (!accountIds.Any(x => string.Equals(x?.Trim(), order.AccountId, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                return;
+            }
+
+            await order.AddHubs(warehouse);
+        }
+    }
+}
