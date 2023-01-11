5e848fd8eaa599ebb65e8cdf2e35ab55e1049319
aproteau <alex.proteau@dispatchscience.com>
2023-01-11 20:51:51 +0000

    AUTO: Copying from DS Repo

---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddExtraFeeForWorkflow.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample.Examples
+{
+    public class AddExtraFeeForWorkflow : IOrderUpdateScript
+    {
+        public async Task OnOrderUpdate(IOrderUpdater order, IScriptDataProvider data, ILogger logger)
+        {    
+            var hasSignatureWf = order.DeliveryWorkflow?.Steps.Any(x => x.IsActive && x.StepType == Workflow.Step.Type.Signature);
+            var extraFeeQty = (hasSignatureWf.HasValue && hasSignatureWf.Value) ? 1 : 0;
+            var sigantureExtraFee = order.ExtraFees.FirstOrDefault(e => string.Equals(e.ExtraFeeTypeId, "signature_required", StringComparison.InvariantCultureIgnoreCase));
+            if (sigantureExtraFee == null)
+            {
+                await order.AddExtraFee("Signature_Required", extraFeeQty);
+            }
+            else if (extraFeeQty != sigantureExtraFee.Quantity)
+            {
+                await order.UpdateExtraFee(sigantureExtraFee.ChargeId, extraFeeQty);
+            }
+        }
+    }
+}
---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
