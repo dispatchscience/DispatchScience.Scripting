1a973483e6b5bab76983cbadb422b83c86a75c54
aproteau <alex.proteau@dispatchscience.com>
2023-01-11 20:54:16 +0000

    AUTO: Copying from DS Repo

---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddTimeToDeliverOnOrderUpdate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample.Examples
+{
+    public class AddTimeToDeliverOnOrderUpdate : IOrderUpdateScript
+    {
+        public async Task OnOrderUpdate(IOrderUpdater order, IScriptDataProvider data, ILogger logger)
+        {
+            if (order?.EventDates == null)
+            {
+                logger.LogInformation("No events");
+                return;
+            }
+
+            if (!order.EventDates.Delivered.HasValue || !order.EventDates.PickedUp.HasValue)
+            {
+                logger.LogInformation("No events dates");
+                return;
+            }
+
+            logger.LogInformation("Calculating time...");
+            var delivered = order.EventDates.Delivered.Value;
+            var pickup = order.EventDates.PickedUp.Value;
+
+            var timeToDoDelivery = delivered.Subtract(pickup);
+            var chargeIncrement = 15;
+            var minutesToCharge = Convert.ToDecimal((int)(Math.Ceiling(timeToDoDelivery.TotalMinutes / chargeIncrement) * chargeIncrement));
+            logger.LogInformation("Minutes to charge :" + minutesToCharge.ToString());
+            if (minutesToCharge > 0)
+            {
+                var dailyHoursCharge = order.ExtraFees.FirstOrDefault(e => e.ExtraFeeTypeId == "DailyHours");
+                if (dailyHoursCharge == null)
+                {
+                    logger.LogInformation("Added");
+                    await order.AddExtraFee("DailyHours", minutesToCharge);
+                }
+                else
+                {
+                    logger.LogInformation("Updated");
+                    await order.UpdateExtraFee(dailyHoursCharge.ChargeId, minutesToCharge);
+                }
+            }
+        }
+    }
+}
---
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/PriceByParcelExtraFee.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dispatch.Scripts.Sample.Examples
+{
+    public class PriceByParcelExtraFee : IExtraFeeScript
+    {
+        //Originally created for tenant Mailpak
+        //Calculate an extra fee based on all the parcels on the order. Each parcel type can have a different price.
+        public async Task<ExtraFeeScriptResult> GetExtraFeePriceInfo(OrderScriptInfo order, IExtraFeeScriptFile file, IScriptDataProvider data, ILogger logger)
+        {
+            var sheetValues = await file.GetSheet();
+
+            var priceParcels = sheetValues.GroupBy(x => x.RowName);
+
+            var priceByParcelTypes = new List<(string ParcelTypeId, decimal Price, decimal Extra)>();
+            foreach (var parcelTypeValues in priceParcels)
+            {
+                var parcelTypeId = parcelTypeValues.Key;
+                var extra = decimal.Parse(sheetValues.FirstOrDefault(x => x.RowName == parcelTypeId && string.Equals(x.ColumnName, "extra", StringComparison.InvariantCultureIgnoreCase))?.CellValue);
+                var price = decimal.Parse(sheetValues.FirstOrDefault(x => x.RowName == parcelTypeId && string.Equals(x.ColumnName, "price", StringComparison.InvariantCultureIgnoreCase))?.CellValue);
+                priceByParcelTypes.Add((parcelTypeId, price, extra));
+            }
+
+            decimal totalPrice = 0;
+            foreach (var item in priceByParcelTypes.OrderByDescending(x => x.Price))
+            {
+                var orderItemsOfParcelType = order.OrderItemInfos.Where(x => string.Equals(x.ParcelTypeId, item.ParcelTypeId, StringComparison.InvariantCultureIgnoreCase)).ToList();
+                if (orderItemsOfParcelType.Any())
+                {
+                    if (totalPrice == 0)
+                    {
+                        totalPrice = item.Price;
+                        totalPrice += item.Extra * (orderItemsOfParcelType.Count - 1);
+                    }
+                    else
+                    {
+                        totalPrice += item.Extra * orderItemsOfParcelType.Count;
+                    }
+                }
+            }
+
+            return new ExtraFeeScriptResult
+            {
+                Quantity = 1,
+                UnitPrice = totalPrice
+            };
+        }
+    }
+}
