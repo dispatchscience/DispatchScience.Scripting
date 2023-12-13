f8c5b5c27ac5affcf77ffe32cdfaa71859f0ed50
NIkolay Hristov <nikolay.hristov@apcurium.com>
2023-12-13 17:48:02 -0500

    AUTO: Copying from DS Repo

--- Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddExtraFeeForWorkflow.cs
+++ Dispatch.Scripts.Sample/Dispatch.Scripts.Sample/Examples/AddExtraFeeForWorkflow.cs
@@ -9,7 +9,7 @@ namespace Dispatch.Scripts.Sample.Examples
     {
         public async Task OnOrderUpdate(IOrderUpdater order, IScriptDataProvider data, ILogger logger)
         {    
-            var hasSignatureWf = order.DeliveryWorkflow?.Steps.Any(x => x.IsActive && x.StepType == Workflow.Step.Type.Signature);
+            var hasSignatureWf = order.DeliveryWorkflow?.Steps.Any(x => x.IsActive && x.StepType == WorkflowStepType.Signature);
             var extraFeeQty = (hasSignatureWf.HasValue && hasSignatureWf.Value) ? 1 : 0;
             var sigantureExtraFee = order.ExtraFees.FirstOrDefault(e => string.Equals(e.ExtraFeeTypeId, "signature_required", StringComparison.InvariantCultureIgnoreCase));
             if (sigantureExtraFee == null)
