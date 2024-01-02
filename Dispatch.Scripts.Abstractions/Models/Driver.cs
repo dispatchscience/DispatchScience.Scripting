d6a790278f7310774a22b3941a005c100dcb73cc
Tony Apcurium <tonyapcurium@Mac-mini-M1.local>
2024-01-02 09:26:30 -0500

    AUTO: Copying from DS Repo

--- Dispatch.Scripts.Abstractions/Models/Driver.cs
+++ Dispatch.Scripts.Abstractions/Models/Driver.cs
@@ -1,9 +1,19 @@
-﻿namespace Dispatch.Scripts
+﻿using System;
+
+namespace Dispatch.Scripts
 {
     public class Driver
     {
         public string Id { get; set; } = default!;
-        public string Name { get; set; } = default!;
+
+        /// <summary>
+        /// May not be set depending on the context in which the script is executed.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// May not be set depending on the context in which the script is executed.
+        /// </summary>
         public string? Number { get; set; }
     }
 }
