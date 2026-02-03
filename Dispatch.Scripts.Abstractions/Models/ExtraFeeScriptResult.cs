802d19b42603cd3ba7b49604cec53a5cc28608e5
Mathieu Savaria <mathieu.savaria@dispatchscience.com>
2026-02-03 07:16:47 -0500

    AUTO: Copying from DS Repo

--- Dispatch.Scripts.Abstractions/Models/ExtraFeeScriptResult.cs
+++ Dispatch.Scripts.Abstractions/Models/ExtraFeeScriptResult.cs
@@ -18,5 +18,17 @@ namespace Dispatch.Scripts
         public string? DescriptionPrimary { get; set; }
         public string? DescriptionSecondary { get; set; }
         public IDictionary<string, object?>? Metadata { get; set; }
+        public PriceBreakdownItem[]? PriceBreakdownItems { get; set; }
+    }
+
+    public class PriceBreakdownItem
+    {
+        public string? TitlePrimary { get; set; }
+        public string? TitleSecondary { get; set; }
+        public decimal? Quantity { get; set; }
+        public string? UnitOfMeasurePrimary { get; set; }
+        public string? UnitOfMeasureSecondary { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? TotalPrice { get; set; }
     }
 }
